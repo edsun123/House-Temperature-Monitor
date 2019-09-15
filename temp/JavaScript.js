83a1ce2c0ce2242e1b78a626ab8fc8cb772f73b8
edsun123 <34005715+edsun123@users.noreply.github.com>
2019-09-15 14:47:20 -0400

    Add files via upload

---
+++ temp/JavaScript.js
@@ -0,0 +1,53 @@
+﻿function add(num1, num2) {
+	return num1 + num2;
+}
+
+window.addEventListener('load', () => {
+    let temperatureDescription = document.querySelector('.description');
+    let humidityDescription = document.querySelector('.humidity-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            //42.351900, -71.122884
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+
+            const api = `${proxy}https://api.darksky.net/forecast/83288ff7de11bebbc519f5408253800b/${lat},${long}';
+
+            fetch(api)
+                .then(response=> {
+                    return response.json();
+                })
+                .then(data => {
+                    
+                    console.log(data);
+                    const { temperature, summary , icon, humidity} = data.currently;
+                    temperatureDegree.textContent = temperature;
+                    
+                    temperatureDescription.textContent = summary;
+                    
+                    locationTimezone.textContent = data.timezone;
+                    
+                    setIcons(icon, document.querySelector('.icon'));
+
+                    humidityDescription.textContent = humidity*100;
+                });
+        });
+
+       
+            
+    }
+    else {
+        hi.textContent = "Geolocation needs to be enabled";
+    }
+
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({ color: "white" });
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+});
---
+++ temp/Login.js
@@ -0,0 +1,21 @@
+﻿//function onSignIn(googleUser) {
+//    console.log("Succesfully Singed in!!!");
+    
+//}
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $(".g-signin2").css("display", "none");
+    $(".right").css("display", "block");
+    $(".welcome").css("display", "none");
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        alert("You've been signed out");
+
+        $(".g-signin2").css("display", "block");
+        $(".right").css("display", "none");
+        $(".welcome").css("display", "block");
+    });
+}
